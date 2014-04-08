3a31b5b6917430791837d6d7d5fda4932371cb8a
David Lu!! <davidvlu@gmail.com>
2014-04-07 20:52:44 -0500

    Rearrange code in clearance layer

--- clearance_layer/include/clearance_layer/clearance_layer.h
+++ clearance_layer/include/clearance_layer/clearance_layer.h
@@ -14,22 +14,8 @@ class ClearanceLayer : public costmap_2d::InflationLayer
 public:
   ClearanceLayer();
   
-  virtual unsigned char computeCost(double distance) const
-  {
-    unsigned char cost = 0;
-    if (distance == 0)
-      cost = costmap_2d::LETHAL_OBSTACLE;
-    else if (distance * resolution_ <= inscribed_radius_)
-      cost = costmap_2d::INSCRIBED_INFLATED_OBSTACLE;
-    else
-    {
-      double euclidean_distance = distance * resolution_;
-      double factor = weight_ / 100. * (1.0 - (euclidean_distance - inscribed_radius_) / (inflation_radius_ - inscribed_radius_));
-      //double factor = exp(-1.0 * weight_ * (euclidean_distance - inscribed_radius_));
-      cost = (unsigned char)((costmap_2d::INSCRIBED_INFLATED_OBSTACLE - 1) * factor);
-    }
-    return cost;
-  }
+  virtual unsigned char computeCost(double distance) const;
+
   
 };
 }
--- clearance_layer/src/clearance_layer.cpp
+++ clearance_layer/src/clearance_layer.cpp
@@ -11,5 +11,20 @@ namespace clearance_layer
 
 ClearanceLayer::ClearanceLayer() {}
 
+unsigned char ClearanceLayer::computeCost(double distance) const
+{
+    unsigned char cost = 0;
+    if (distance == 0)
+        cost = costmap_2d::LETHAL_OBSTACLE;
+    else if (distance * resolution_ <= inscribed_radius_)
+        cost = costmap_2d::INSCRIBED_INFLATED_OBSTACLE;
+    else
+    {
+        double euclidean_distance = distance * resolution_;
+        double factor = weight_ / 100. * (1.0 - (euclidean_distance - inscribed_radius_) / (inflation_radius_ - inscribed_radius_));
+        cost = (unsigned char)((costmap_2d::INSCRIBED_INFLATED_OBSTACLE - 1) * factor);
+    }
+    return cost;
+}
 
 } // end namespace
